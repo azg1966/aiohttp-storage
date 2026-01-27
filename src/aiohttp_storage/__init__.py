d836402508cb87089d4e36e51fe8401de96e3048
Alex G. <some@email>
2026-01-27 15:33:54 +0400

    Import fix

--- src/aiohttp_storage/__init__.py
+++ src/aiohttp_storage/__init__.py
@@ -8,17 +8,19 @@ from .storage import (
     file_exists,
     delete_file,
     get_storage,
+    remove_directory,
     FILE_STORAGE_APP_KEY,
 )
 
 __all__ = [
-    'AbstractStorage',
-    'FileSystemStorage',
-    'get_valid_filename',
-    'setup',
-    'save_file',
-    'file_exists',
-    'delete_file',
-    'get_storage',
-    'FILE_STORAGE_APP_KEY'
+    "AbstractStorage",
+    "FileSystemStorage",
+    "get_valid_filename",
+    "setup",
+    "save_file",
+    "file_exists",
+    "delete_file",
+    "get_storage",
+    "remove_directory",
+    "FILE_STORAGE_APP_KEY",
 ]
